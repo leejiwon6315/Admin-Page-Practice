fbc7c9bef8bef5a33ff1204fffb67c71feab66fb
leejiwon6315 <leejiwon6315@naver.com>
2020-09-13 03:22:14 +0900

    App.js -> changed arrow func 그리고 bug 미해결

--- src/App.js
+++ src/App.js
@@ -7,20 +7,10 @@ import Login from "./AuthInfo/Login";
 import "./App.module.css";
 import SignIn from "./AuthInfo/Auth";
 
-function App() {
-  // Link를 사용하기 위해서는 Router 태그 내부에 있어야함
-
+const App = () => {
   const login = ({ userId, password }) => SignIn({ userId, password });
 
-  return (
-    // <HashRouter>
-    //   <Navigation />
-
-    //   <Route path="/" exact={true} component={MainSection} />
-    //   <Route path="/reservation" component={Reservation} />
-    // </HashRouter>
-    <Login login={login} />
-  );
-}
+  return <Login login={login} />;
+};
 
 export default App;
